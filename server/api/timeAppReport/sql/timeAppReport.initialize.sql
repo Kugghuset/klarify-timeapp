edca79b7eaa5f00057d0bb508ba9c7c83391ded8
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2016-07-26 14:03:01 +0200

    Add columns to _FactKugghuset.
    - TimeAppReportId is needed for matching against the [TimeAppReport] table
    - FactKugghusetID is needed to ensure there is some sort of uniqueness amongst the columns
    
    Note that before going live, update all [_FactKugghuset] to [FactKugghuset].

--- server/api/timeAppReport/sql/timeAppReport.initialize.sql
+++ server/api/timeAppReport/sql/timeAppReport.initialize.sql
@@ -24,3 +24,31 @@ BEGIN
     , [isDisabled] Bit DEFAULT 0 NULL -- Used for determining existance
   )
 END
+
+/********************************************************************
+ * Ensures _FactKugghuset is up to par with what the system requires.
+ ********************************************************************/
+
+/**
+ * Ensure [TimeAppReportId] exists on _FactKugghuset
+ * by adding it if it doesn't exist.
+ */
+IF NOT EXISTS(SELECT * FROM sys.columns
+              WHERE Name = N'TimeAppReportId'
+                AND Object_ID = Object_ID(N'_FactKugghuset'))
+BEGIN
+  ALTER TABLE [dbo].[_FactKugghuset]
+  ADD [TimeAppReportId] BigInt NULL
+END
+
+/**
+ * Ensure [FactKugghusetID] exists on _FactKugghuset
+ * by adding it if it doesn't exist.
+ */
+IF NOT EXISTS(SELECT * FROM sys.columns
+              WHERE Name = N'FactKugghusetID'
+                AND Object_ID = Object_ID(N'_FactKugghuset'))
+BEGIN
+  ALTER TABLE [dbo].[_FactKugghuset]
+  ADD [FactKugghusetID] BigInt IDENTITY(1, 1) PRIMARY KEY NOT NULL
+END
